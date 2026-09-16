ffb21de7b6c8ab597c3db7543a1a0bc32f7dda70
Nishu Kumarage <nishukumarage2025@gmail.com>
2026-09-16 18:24:52 +0530

    Add hospital lookup and bed data

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_SPECIALTIES 4
+#define NUM_WARDS 4
+
+char specialtyName [NUM_SPECIALTIES][50]= {"General Practice (OPD)","Paediatrics","Cardiology","Neurology"};
+int consultationFee[NUM_SPECIALTIES] = {1500,2500,4500,5000};
+int consultationTime [NUM_SPECIALTIES] = {15,20,30,30};
+int dailyPatientCap [NUM_SPECIALTIES]={30,20,12,10};
+
+char wardName [NUM_WARDS][50]= {"General Ward","Paediatric Ward","Surgical Ward","ICU (Intensive Care Unit)"};
+int dailyBedRate [NUM_WARDS]={3000,6000,12000,25000};
+int totalBedCap [NUM_WARDS]={20,10,10,5};
+
+int bedOccupancy [NUM_WARDS][20];
+
 int main()
+
 {
-    printf("Hello world!\n");
+    printf("Smart Hospital & Resource Allocation System\n");
     return 0;
 }
